cbc8f8e33c1b729633aa73d75ea34f1f6246c3a7
Lillegul <camillal1@live.no>
2025-03-30 15:07:06 +0200

    .

--- js/shopping.js
+++ js/shopping.js
@@ -3,7 +3,7 @@ import db from "./db.js"
 console.log("start")
 
 async function rerenderList() {
-  const items = await db.listItems()
+  const items = await db.listShoppingItems()
 
   const ul = document.getElementById("uncheckedItemsShoppingmode")
 
@@ -11,19 +11,20 @@ async function rerenderList() {
     const li = document.createElement("li")
     li.className = "checklistItem"
     li.id = index
+    const checked = item.checked ? "checked" : ""
 
     li.innerHTML = `<span id="itemName">
-    ${item.name} 
+    ${item.name}
   </span>
   <span id="itemAmount">
    x ${item.amount} 
   </span> 
 
-  <input class="checkbox" type="checkbox" name="checkbox" id="${index}"> `
+  <input class="checkbox" type="checkbox" name="checkbox" id="${index}" ${checked}> `
     ul.appendChild(li)
   })
 }
-
+//  - ${item.category} - ${item.categoryColor}
 await rerenderList()
 
 console.log("end")
