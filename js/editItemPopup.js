5141c2cb037c38d041903625fd4b1c2679d3c5d6
Lillegul <camillal1@live.no>
2025-04-04 16:15:50 +0200

    fixed modal and cleaned code

---
+++ js/editItemPopup.js
@@ -0,0 +1,29 @@
+// import db from "./db.js"
+
+
+
+
+
+
+
+// const categories = await db.listCategories()
+// console.table(categories)
+
+// function displayCategories() {
+//   const fieldset = document.getElementById("categoryListFieldset")
+//   fieldset.innerHTML = categories.map((category) => {
+//     return createCategoryItem(category.name, category.id, category.color)
+//   }).join ('')
+// }
+
+
+// function createCategoryItem(name, id, color) {
+//   return `
+
+//    <input type="radio" id="${id}" name="Categories" value="${name}" />
+   
+//    <label for="${id}" style="border-color: ${color}; background-color: ${color};" >${name}</label>`
+// }
+// displayCategories()
+
+// console.log(listCategories())
--- js/planner.js
+++ js/planner.js
@@ -1,36 +1,54 @@
 import db from "./db.js"
 
 console.log("start")
+await rerenderList()
 
+
+// rerender list
 async function rerenderList() {
   const items = await db.listItems()
 
   const ul = document.getElementById("uncheckedItemsPlanning")
-
+  // onclick="openEdit('${item.id}')    "
   items.forEach((item, index) => {
     const li = document.createElement("li")
     li.className = "checklistItem"
     li.id = index
     const checked = item.checked ? "checked" : ""
-    li.innerHTML = `<span id="itemName">
-    ${item.name} 
-  </span>        
-  <button class="listItemBtn" id="itemAmount" onclick="openEdit('${item.id}')"> 
-        x ${item.amount} 
-      </button>
-
-
-  <input onclick="selectOutput1(id)" class="checkbox" type="checkbox" name="checkbox" id="l_${item.id}" ${checked}> `
+    li.innerHTML = `<span  id="itemName"> ${item.name} </span>        
+    <button data-open-modal class="listItemBtn" id="itemAmount" onclick="document.querySelector('[data-modal]').showModal()"> 
+        x ${item.amount} </button>
+    <input onclick="selectOutput1(id)" class="checkbox" type="checkbox" name="checkbox" id="l_${item.id}" ${checked}> `
     ul.appendChild(li)
   })
 }
 
-await rerenderList()
+//open/close edit modal
+const openEditModal = document.querySelector("[data-open-modal]")
+const closeEditModal = document.querySelector(".submitPageBtn")
+const editModal = document.querySelector("[data-modal]")
+closeEditModal.returnValue = "favAnimal"
 
-console.log("end")
+// need big help
+openEditModal.addEventListener("click", () => {
+  console.log("modal works")
+  document.getElementById("ItemInputname").value = item.name
+  //  input.innerHTML.value = ${item.name}
+  editModal.showModal()
+})
 
+closeEditModal.addEventListener("click", () => {
+  editModal.close()
+})
+
+// modal test slutt
+
+console.log(editModal)
+console.log("hello")
+
+
+// brukes denne fotsatt? dont think so?
 let editId = null
-document.getElementById("popUpEdit").style.display = "none"
 
 window.openEdit = openEdit
 console.log("open edit works")
@@ -57,6 +75,45 @@ async function openEdit(id) {
   }
 }
 
+
+// const categoryOptions = document.querySelectorAll("input[name='categories']")
+// const findSelected = () => {
+//   const selected =document.querySelector("input[name='categories']:checked").value
+// }
+// console.log(findSelected());
+
+
+//  my test, dosent work
+// async function getTotalUnchecked () {
+//   const items= await db.listItems()
+//   const item = items.filter((item) => {
+//    return item.checked === true
+
+   
+ 
+
+    // console.log(item.checked)
+  // })
+  // const length= unchecked.length 
+  // console.log(item) 
+  // console.log(checked)
+  // return length
+ 
+  // console.log( length)
+  
+// }
+// getTotalUnchecked()
+// // totalUnchecked.reduce((item) => {
+// // const Unchecked = item.length})
+
+
+// console.log("it does actually work")
+
+
+
+
+
+// add amount to list. dosent work
 const amountInput = document.getElementById("amountInput")
 
 window.sub1 = sub1
@@ -93,18 +150,19 @@ searchbar.onkeyup = async () => {
   }
   display(result)
 }
-// searchOutput.innerHTML = "";
 
+//shows list element
 function display(result) {
   const content = result.map((item) => {
     const checked = item.checked ? "checked" : ""
-    return `<li onclick="selectOutput('${item.id}')">  
-    <input  class="checkbox1" type="checkbox" name="checkbox" diasbled id="s_${item.id}" ${checked}> ${item.name}  </li>`
+    return `<li data-open-modal onclick="selectOutput('${item.id}')">  
+    <input  class="checkbox1" type="checkbox" name="checkbox" diasbled id="s_${item.id}" ${checked}> ${item.name}     
+  </li>`
   })
-  // searchOutput.innerHTML = `<ul> <li> Brød </li> </ul>`
   searchOutput.innerHTML = `<ul> ${content.join("")} </ul>`
 }
 
+// makes checkmarks comunicate across
 window.selectOutput = selectOutput
 
 async function selectOutput(id) {
@@ -121,8 +179,8 @@ async function selectOutput(id) {
   }
 }
 
-window.selectOutput1=selectOutput1
-// fix this
+window.selectOutput1 = selectOutput1
+// fix this: delete and fix the one above
 async function selectOutput1(id) {
   const plannerlistCheckbox = document.getElementById(`l_${id}`)
   const searchCheckbox = document.getElementById(`s_${id}`)
@@ -137,7 +195,31 @@ async function selectOutput1(id) {
   }
 }
 
-
 searchbar.addEventListener("enter", addToUnchecked)
 
 function addToUnchecked() {}
+
+
+
+
+//  CATEGORIES
+
+const categories = await db.listCategories()
+console.table(categories)
+
+function displayCategories() {
+  const fieldset = document.getElementById("categoryListFieldset")
+  fieldset.innerHTML = categories
+    .map((category) => {
+      return createCategoryItem(category.name, category.id, category.color)
+    })
+    .join("")
+}
+
+function createCategoryItem(name, id, color) {
+  return `
+   <input type="radio" id="${id}" name="categories" value="${name}" />
+   
+   <label for="${id}" style="border-color: ${color}; background-color: ${color};" >${name}</label>`
+}
+displayCategories()
